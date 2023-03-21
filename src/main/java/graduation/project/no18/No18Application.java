3b7c9de0055c45c6e2851679b24cae0d3f62975e
jihoon-gh <jihoon806@gmail.com>
2023-03-21 20:19:23 +0900

    FEAT : project init

---
+++ src/main/java/graduation/project/no18/No18Application.java
@@ -0,0 +1,13 @@
+package graduation.project.no18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class No18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(No18Application.class, args);
+	}
+
+}
---
+++ src/test/java/graduation/project/no18/No18ApplicationTests.java
@@ -0,0 +1,13 @@
+package graduation.project.no18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class No18ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
